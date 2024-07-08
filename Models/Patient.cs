e7c78ddb439d08859c98349d314e9a74bce6c037
Ishwari Gholap <ishwarigholap2001@gmail.com>
2024-07-08 19:01:23 +0530

    Patient CRUD Module

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -27,6 +27,6 @@ namespace HospitalManagementSystem.Models
         [Required]
         public DateTime DateOfBirth { get; set; }
 
-        public ICollection<Appointment> Appointments { get; set; }
+        public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
     }
 }
